8f304fb49f1f7c3b46698c0138e39dc66b2d3125
Alioune <aliounealdembaseck@gmail.com>
2023-06-15 18:17:52 +0000

    création de mon guard

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { TokenService } from '../services/token.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+      constructor(private tok:TokenService,private route:Router){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.tok.isLogged()) {
+        
+        return true;
+      }else{
+        this.route.navigate(["/security/login"])
+        return false;
+      }
+  }
+  
+}
